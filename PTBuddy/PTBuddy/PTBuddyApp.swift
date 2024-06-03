31f0a45a0e62d4084edaad708aef338008f77416
doyeonjeong <debby_@kakao.com>
2024-06-03 16:51:51 +0900

    🎉 :: project init

---
+++ PTBuddy/PTBuddy/PTBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PTBuddyApp.swift
+//  PTBuddy
+//
+//  Created by doyeonjeong on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PTBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
