f1662d0b3d0525ee3355fdee1e2dc4ddc9f7eb0b
jhpa0308 <jhpa0308@gmail.com>
2024-06-05 11:08:26 +0900

    ➕ :: 파일정리 #3

---
+++ PTBuddy/PTBuddy/ActivityTab/ActivityMainView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityMainView.swift
+//  PTBuddy
+//
+//  Created by Ji Hye PARK on 6/5/24.
+//
+
+import SwiftUI
+
+struct ActivityMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityMainView()
+}
---
+++ PTBuddy/PTBuddy/OnboardingTab/OnboardingMainView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingMainView.swift
+//  PTBuddy
+//
+//  Created by Ji Hye PARK on 6/5/24.
+//
+
+import SwiftUI
+
+struct OnboardingMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingMainView()
+}
