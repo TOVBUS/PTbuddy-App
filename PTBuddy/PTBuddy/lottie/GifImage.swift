eb865fe918befeed283990888937422a12fd4a18
HyeS00 <hyesookim1118@gmail.com>
2024-06-13 15:42:03 +0900

    ✨ :: lottie 로직 및loading 이미지 추가

---
+++ PTBuddy/PTBuddy/lottie/GifImage.swift
@@ -0,0 +1,18 @@
+//
+//  GifImage.swift
+//  PTBuddy
+//
+//  Created by 김혜수 on 6/13/24.
+//
+
+import SwiftUI
+
+struct GifImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GifImage()
+}
