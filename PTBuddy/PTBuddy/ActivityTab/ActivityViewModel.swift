6d29f86853ceaa2642aecde08653500db47a8df9
doyeonjeong <debby_@kakao.com>
2024-06-14 11:54:58 +0900

    🎨 :: 주간 운동 루틴 불러오기 - dummy

--- PTBuddy/PTBuddy/ActivityTab/ActivityViewModel.swift
+++ PTBuddy/PTBuddy/ActivityTab/ActivityViewModel.swift
@@ -11,7 +11,13 @@ import Alamofire
 
 class ActivityViewModel: ObservableObject {
     @Published var activities: [Activity] = []
-    @Published var routines: [Routine] = []
+    @Published var routines: [Routine] = [
+        Routine(id: UUID().uuidString, day: "월요일", exerciseArea: "전신 유산소", exerciseNameEN: "Treadmill Running", exerciseNameKR: "트레드밀 달리기", reps: nil, sets: nil, weight: nil, duration: "3600"),
+        Routine(id: UUID().uuidString, day: "화요일", exerciseArea: "전신 유산소", exerciseNameEN: "Jumping Jacks", exerciseNameKR: "점핑잭", reps: "50회", sets: "3세트", weight: nil, duration: nil),
+        Routine(id: UUID().uuidString, day: "수요일", exerciseArea: "하체 근력 훈련 (하체, 복부)", exerciseNameEN: "Squats", exerciseNameKR: "스쿼트", reps: "15회", sets: "3세트", weight: "10kg", duration: nil),
+        Routine(id: UUID().uuidString, day: "목요일", exerciseArea: "하체 근력 훈련 (하체, 복부)", exerciseNameEN: "Lunges", exerciseNameKR: "런지", reps: "12회", sets: "3세트", weight: "10kg", duration: nil),
+        Routine(id: UUID().uuidString, day: "금요일", exerciseArea: "하체 근력 훈련 (하체, 복부)", exerciseNameEN: "Plank", exerciseNameKR: "플랭크", reps: nil, sets: "3세트", weight: nil, duration: "60")
+    ]
     @Published var weeklyGoal: Int = 7
     @Published var completedWorkouts: Int = 3
     
@@ -29,20 +35,20 @@ class ActivityViewModel: ObservableObject {
     // TODO: 온보딩 서비스에서 호출하면 이거 필요없음
     func fetchRoutines() {
         let url = "http://localhost:3000/routine/generate"
-//        let parameters: [String: Any] = [ ? ]
+        //        let parameters: [String: Any] = [ ? ]
         
-//        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
-//            .responseDecodable(of: RoutineResponse.self) { response in
-//                switch response.result {
-//                    case .success(let routineResponse):
-//                        let parsedRoutines = self.parseResponse(response: routineResponse.responses)
-//                        DispatchQueue.main.async {
-//                            self.routines = parsedRoutines
-//                        }
-//                    case .failure(let error):
-//                        print(error)
-//                }
-//            }
+        //        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+        //            .responseDecodable(of: RoutineResponse.self) { response in
+        //                switch response.result {
+        //                    case .success(let routineResponse):
+        //                        let parsedRoutines = self.parseResponse(response: routineResponse.responses)
+        //                        DispatchQueue.main.async {
+        //                            self.routines = parsedRoutines
+        //                        }
+        //                    case .failure(let error):
+        //                        print(error)
+        //                }
+        //            }
     }
     
     private func parseResponse(response: String) -> [Routine] {
